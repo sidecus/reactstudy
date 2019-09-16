f9cd32620960a4050de86ba7c0bb6b05c8311417
sidecus <4399408+sidecus@users.noreply.github.com>
2019-09-16 16:59:31 -0700

    small update

--- src/WordCloud/trumptweets.tsx
+++ src/WordCloud/trumptweets.tsx
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './trumptweets.css';
 import { QueryCloud } from './querycloud';
 
+export const numberOfTweets = 800;
+export const maxFontSize = 80;
+export const wordCount = 60;
+
 export const TrumpTweets = () => {
     const [queries, setQueries] = useState<string[]>([]);
 
     useEffect(() => {
         const getTrumpTweets = async () => {
             const trumpTweets = [] as string[];
+            // data from http://trumptwitterarchive.com/
             const url = 'http://d5nxcu7vtzvay.cloudfront.net/data/realdonaldtrump/2018.json';
             const response = await fetch(url);
             const tweets = await response.json();
-            tweets.slice(0, 500).map((t: any) => trumpTweets.push(t.text.replace('&amp', '&')));
+            tweets.slice(0, numberOfTweets).map((t: any) => trumpTweets.push(t.text.replace('&amp;', '&')));
 
             setQueries(trumpTweets);
         };
@@ -22,8 +27,8 @@ export const TrumpTweets = () => {
 
     return (
         <div className="clouddiv">
-            <h3>Trump 2018 last 500 tweets</h3>
-            <QueryCloud queries={queries} wordCount={80} maxFontSize={70}/>
+            <h3>Trump 2018 last {numberOfTweets} tweets</h3>
+            <QueryCloud queries={queries} wordCount={wordCount} maxFontSize={maxFontSize}/>
         </div>
     );
 };

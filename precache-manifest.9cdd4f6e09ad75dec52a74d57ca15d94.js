2d88663f10e96b085c37ebe0c00b272e3099e351
Zhengwen Zhu <zhengwen.zhu@live.com>
2019-09-24 02:08:40 -0700

    Updates

---
+++ precache-manifest.9cdd4f6e09ad75dec52a74d57ca15d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e761a41e8b96b6008fd6254df8b4c4",
+    "url": "/reactstudy/index.html"
+  },
+  {
+    "revision": "1667786da9894a0242a6",
+    "url": "/reactstudy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "76f482185e7a12462b66",
+    "url": "/reactstudy/static/js/2.073a7c90.chunk.js"
+  },
+  {
+    "revision": "1667786da9894a0242a6",
+    "url": "/reactstudy/static/js/main.ae295e5c.chunk.js"
+  },
+  {
+    "revision": "6c4035e4f00b15ce1cfd",
+    "url": "/reactstudy/static/js/runtime~main.c70b4494.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactstudy/static/media/logo.5d5d9eef.svg"
+  }
+]);

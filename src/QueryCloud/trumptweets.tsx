42bd13c03f6900c01601c5bd8f5611ad8cc9d06b
sidecus <4399408+sidecus@users.noreply.github.com>
2019-09-24 02:21:39 -0700

    enable githubio publish

--- src/QueryCloud/trumptweets.tsx
+++ src/QueryCloud/trumptweets.tsx
@@ -39,7 +39,7 @@ export const TrumpTweets = () => {
         const getTrumpTweets = async () => {
             const trumpTweets = [] as string[];
             // data from http://trumptwitterarchive.com/
-            const url = 'http://d5nxcu7vtzvay.cloudfront.net/data/realdonaldtrump/2018.json';
+            const url = 'https://d5nxcu7vtzvay.cloudfront.net/data/realdonaldtrump/2018.json';
             const response = await fetch(url);
             const tweets = await response.json();
             tweets.slice(0, numberOfTweets).map((t: any) => trumpTweets.push(t.text.replace('&amp;', '&')));
